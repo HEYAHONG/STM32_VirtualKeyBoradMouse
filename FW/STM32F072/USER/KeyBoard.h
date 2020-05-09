b2e1342431a3bb11c831326241ecdcddbb51191f
HYH <2229388563@qq.com>
2020-05-09 09:32:34 +0800

    修改鼠标逻辑，添加新代码

--- FW/STM32F072/USER/KeyBoard.h
+++ FW/STM32F072/USER/KeyBoard.h
@@ -8,7 +8,6 @@
 #ifndef USER_KEYBOARD_H_
 #define USER_KEYBOARD_H_
 #include "stdint.h"
-
 typedef union
 {
 uint8_t byte;
@@ -25,6 +24,115 @@ uint8_t Right_GUI:1;
 } key;
 } KB_Special_Key;//特殊按键定义
 
+//一般按键的枚举变量0x00(No Event)到0x65(APP)
+typedef enum
+{
+	NO_EVENT=0x00,
+	Overrun_Error=0x01,
+	POST_Fail=0x02,
+	ErrorUndefined=0x03,
+	a_A=0x04,
+	b_B=0x05,
+	c_C=0x06,
+	d_D=0x07,
+	e_E=0x08,
+	f_F=0x09,
+	g_G=0x0a,
+	h_H=0x0b,
+	i_I=0x0c,
+	j_J=0x0d,
+	k_K=0x0e,
+	l_L=0x0f,
+	m_M=0x10,
+	n_N=0x11,
+	o_O=0x12,
+	p_P=0x13,
+	q_Q=0x14,
+	r_R=0x15,
+	s_S=0x16,
+	t_T=0x17,
+	u_U=0x18,
+	v_V=0x19,
+	w_W=0x1a,
+	x_X=0x1b,
+	y_Y=0x1c,
+	z_Z=0x1d,
+	Key_num_1=0x1e,//1!
+	Key_num_2=0x1f,//2@
+	Key_num_3=0x20,//3#
+	Key_num_4=0x21,//4$
+	Key_num_5=0x22,//5%
+	Key_num_6=0x23,//6^
+	Key_num_7=0x24,//7&
+	Key_num_8=0x25,//8*
+	Key_num_9=0x26,//9(
+	Key_num_0=0x27,//0)
+	Return=0x28,
+	Escape=0x29,
+	Backspace=0x2A,
+	Tab=0x2B,
+	Space=0x2c,
+	Key_2D=0x2d,//-_
+	Key_2E=0x2e,//=+
+	Key_2F=0x2f,//[{
+	Key_30=0x30,//]}
+	Key_31=0x31,//\|
+	Europe_1=0x32,
+	Key_33=0x33,// ;:
+	Key_34=0x34,// '"
+	Key_35=0x35,// `~
+	Key_36=0x36,//,<
+	Key_37=0x37,//.>
+	Key_38=0x38,///?
+	Caps_Lock=0x39,
+	F1=0x3a,
+	F2=0x3b,
+	F3=0x3c,
+	F4=0x3d,
+	F5=0x3e,
+	F6=0x3f,
+	F7=0x40,
+	F8=0x41,
+	F9=0x42,
+	F10=0x43,
+	F11=0x44,
+	F12=0x45,
+	Print_Screen=0x46,
+	Scroll_Lock=0x47,
+	Break=0x48,//Ctrl-Pause
+	Pause=0x48,
+	Insert=0x49,
+	Home=0x4a,
+	Page_Up=0x4b,
+	Delete=0x4c,
+	End=0x4d,
+	Page_Down=0x4e,
+	Right_Arrow=0x4f,
+	Left_Arrrow=0x50,
+	Down_Arrow=0x51,
+	Up_Arrow=0x52,
+	Num_Lock=0x53,
+	Key_Keypad_54=0x54,// /
+	Key_Keypad_55=0x55,// *
+	Key_Keypad_56=0x56,// -
+	Key_Keypad_57=0x57,// +
+	Key_Keypad_Enter=0x58,
+	Key_Keypad_num_1=0x59,//End
+	Key_Keypad_num_2=0x5a,//Down
+	Key_Keypad_num_3=0x5b,//PageDn
+	Key_Keypad_num_4=0x5c,//Left
+	Key_Keypad_num_5=0x5d,
+	Key_Keypad_num_6=0x5e,//Right
+	Key_Keypad_num_7=0x5f,//Home
+	Key_Keypad_num_8=0x60,//Up
+	Key_Keypad_num_9=0x61,//PageUp
+	Key_Keypad_num_0=0x62,//Insert
+	Key_Keypad_dot=0x63,//delete
+	Europe_2=0x64,
+	App=0x65
+	//HID描述符规定的最大按键为0x65
+
+} KB_Key;
 
 typedef struct
 {
--- FW/STM32F072/USER/KeyScan.c
+++ FW/STM32F072/USER/KeyScan.c
@@ -15,8 +15,8 @@ void keyscan()//扫描按键并且执行动作
 	{//扫描鼠标按键
 		Mouse_Data data;
 		Mouse_Set_Default(&data);
-		bool R_click,M_click,L_cilck,R,L,U,D;
-
+		bool R_click,M_click,L_cilck,R,L,U,D;//按键受否按下的标志
+		static uint32_t R_Step=1,L_Step=1,U_Step=1,D_Step=1;//每次按键前进的步长
 		R_click=M_click=L_cilck=R=L=U=D=false;
 
 		{//第一次扫描
@@ -57,23 +57,63 @@ void keyscan()//扫描按键并且执行动作
 			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_0)!=GPIO_PIN_RESET)
 			{
 					if(U)
-						Mouse_Set_Y(&data,-10);
+					{
+						Mouse_Set_Y(&data,-(U_Step++));
+					}
+					else
+					{
+						U_Step=1;
+					}
+			}
+			else
+			{
+				U_Step=1;
 			}
 
 			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1)!=GPIO_PIN_RESET)
 			{
 					if(D)
-						Mouse_Set_Y(&data,10);
+					{
+						Mouse_Set_Y(&data,D_Step++);
+					}
+					else
+					{
+						D_Step=1;
+					}
+			}
+			else
+			{
+				D_Step=1;
 			}
 			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_14)!=GPIO_PIN_RESET)
 			{
 					if(R)
-						Mouse_Set_X(&data,10);
+					{
+						Mouse_Set_X(&data,R_Step++);
+					}
+					else
+					{
+						R_Step=1;
+					}
+			}
+			else
+			{
+				R_Step=1;
 			}
 			if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_15)!=GPIO_PIN_RESET)
 			{
 					if(L)
-						Mouse_Set_X(&data,-10);
+					{
+						Mouse_Set_X(&data,-(L_Step++));
+					}
+					else
+					{
+						L_Step=1;
+					}
+			}
+			else
+			{
+				L_Step=1;
 			}
 			if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_10)!=GPIO_PIN_RESET)
 			{
